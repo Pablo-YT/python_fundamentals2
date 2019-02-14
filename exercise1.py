048d143a59736aa63a8fde82ae801ced6120eda5
Josh Date <jjdate@live.com>
2019-02-13 22:28:01 -0500

    created the function to double mynumber

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+#Define a function called double that accepts an argument called my_number and returns that number multiplied by 2.
+
+#Try calling it multiple times and pass in different numbers each time.
+
+my_number = 7
+def double(my_number):
+	return print(my_number * 2)
+	
+
+double(my_number)
